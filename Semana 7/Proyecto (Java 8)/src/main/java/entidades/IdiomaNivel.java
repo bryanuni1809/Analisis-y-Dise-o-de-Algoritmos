d9e10ae4dd094f9219669b293680cac336122edd
bryanuni1809 <bryanuni1809@gmail.com>
2025-10-10 19:21:17 -0500

    Arreglos

--- Semana 7/Proyecto (Java 8)/src/main/java/entidades/IdiomaNivel.java
+++ Semana 7/Proyecto (Java 8)/src/main/java/entidades/IdiomaNivel.java
@@ -8,24 +8,24 @@ package entidades;
  *
  * @author BRYAN
  */
-public class IdiomaNivel {
+public class IdiomaNivel{
     private String codigo;
     private String idioma;
     private String nivel;
     private String descripcion;
 
-    public IdiomaNivel(String codigo, String idioma, String nivel, String descripcion) {
-        this.codigo = codigo;
-        this.idioma = idioma;
-        this.nivel = nivel;
-        this.descripcion = descripcion;
+    public IdiomaNivel(String codigo,String idioma,String nivel,String descripcion){
+        this.codigo=codigo;
+        this.idioma=idioma;
+        this.nivel=nivel;
+        this.descripcion=descripcion;
     }
 
-    public String getCodigo() { return codigo; }
-    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getCodigo(){return codigo;}
+    public void setCodigo(String codigo){ this.codigo=codigo;}
 
-    public String getIdioma() { return idioma; }
-    public void setIdioma(String idioma) { this.idioma = idioma; }
+    public String getIdioma(){return idioma;}
+    public void setIdioma(String idioma){this.idioma=idioma;}
 
     public String getNivel() { return nivel; }
     public void setNivel(String nivel) { this.nivel = nivel; }
