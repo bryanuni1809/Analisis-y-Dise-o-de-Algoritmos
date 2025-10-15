92c1f9d85ca459b16d6b9c4a5b619a7a65d02cc0
bryanuni1809 <bryanuni1809@gmail.com>
2025-10-14 22:47:25 -0500

    Arreglos

--- Semana 9/Proyecto (Java 8)/src/main/java/util/BusquedaExterna.java
+++ Semana 9/Proyecto (Java 8)/src/main/java/util/BusquedaExterna.java
@@ -14,14 +14,14 @@ import java.util.List;
  *
  * @author BRYAN
  */
-public class BusquedaExterna {
-     public static List<String> buscarEnArchivo(String archivo, String termino, int columna) throws IOException {
-        List<String> resultados = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+public class BusquedaExterna{
+     public static List<String>buscarEnArchivo(String archivo, String termino, int columna) throws IOException {
+        List<String> resultados=new ArrayList<>();
+        try (BufferedReader br =new BufferedReader(new FileReader(archivo))){
             String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] partes = linea.split(",");
-                if (partes.length > columna && partes[columna].toLowerCase().contains(termino.toLowerCase())) {
+            while ((linea=br.readLine())!=null){
+                String[] partes= linea.split(",");
+                if (partes.length >columna && partes[columna].toLowerCase().contains(termino.toLowerCase())) {
                     resultados.add(linea);
                 }
             }
@@ -29,11 +29,11 @@ public class BusquedaExterna {
         return resultados;
     }
     
-    public static List<String> buscarEnArchivoMultiple(String archivo, String termino) throws IOException {
-        List<String> resultados = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+    public static List<String>buscarEnArchivoMultiple(String archivo, String termino) throws IOException {
+        List<String> resultados=new ArrayList<>();
+        try (BufferedReader br=new BufferedReader(new FileReader(archivo))) {
             String linea;
-            while ((linea = br.readLine()) != null) {
+            while ((linea = br.readLine())!= null) {
                 if (linea.toLowerCase().contains(termino.toLowerCase())) {
                     resultados.add(linea);
                 }
