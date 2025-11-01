7600b46bee7d1a2207ea5e1ac45d865aa3e29e62
bryanuni1809 <bryanuni1809@gmail.com>
2025-10-31 22:54:37 -0500

    Arreglos

--- Semana 11/Practica/Proyecto (Java 8)/src/main/java/gestor/GestorAcademia.java
+++ Semana 11/Practica/Proyecto (Java 8)/src/main/java/gestor/GestorAcademia.java
@@ -60,41 +60,41 @@ public class GestorAcademia{
     inicializarListasInvertidas();
     }
     private void cargarCursos(){
-    try (BufferedReader br=new BufferedReader(new FileReader("cursos.txt"))) {
+    try(BufferedReader br=new BufferedReader(new FileReader("cursos.txt"))){
         String linea;
-        while ((linea = br.readLine()) != null) {
-            String[] partes = linea.split(",");
-            if (partes.length >= 10) {
-                Curso c = new Curso(
-                    partes[0], partes[1], partes[2], partes[3],
-                    partes[4], partes[5], Integer.parseInt(partes[6]),
+        while((linea=br.readLine())!=null){
+            String[]partes=linea.split(",");
+            if(partes.length>=10){
+                Curso c=new Curso(
+                    partes[0],partes[1],partes[2],partes[3],
+                    partes[4],partes[5],Integer.parseInt(partes[6]),
                     Integer.parseInt(partes[7]),
                     Double.parseDouble(partes[8]),
                     partes[9]
                 );
-                cursos.put(c.getCodigo(), c);
+                cursos.put(c.getCodigo(),c);
             }
         }
-    } catch (IOException e) {
-        System.out.println("Error al cargar cursos: " + e.getMessage());
+    }catch(IOException e){
+        System.out.println("Error al cargar cursos: "+e.getMessage());
     }
     }
-    private void cargarProfesores() {
-    try (BufferedReader br = new BufferedReader(new FileReader("profesores.txt"))) {
+    private void cargarProfesores(){
+    try(BufferedReader br=new BufferedReader(new FileReader("profesores.txt"))){
         String linea;
-        while ((linea = br.readLine()) != null) {
-            String[] partes = linea.split(",");
-            if (partes.length >= 8) {
-                Profesor p = new Profesor(
-                    partes[0], partes[1], partes[2], partes[3],
-                    partes[4], partes[5], partes[6],
+        while((linea=br.readLine())!= null){
+            String[]partes=linea.split(",");
+            if (partes.length>=8) {
+                Profesor p=new Profesor(
+                    partes[0],partes[1],partes[2],partes[3],
+                    partes[4],partes[5],partes[6],
                     Integer.parseInt(partes[7])
                 );
-                profesores.put(p.getDni(), p);
+                profesores.put(p.getDni(),p);
             }
         }
-    } catch (IOException e) {
-        System.out.println("Error al cargar profesores: " + e.getMessage());
+    }catch(IOException e){
+        System.out.println("Error al cargar profesores: "+e.getMessage());
     }
 }
     private void cargarNivelesIdioma() {
